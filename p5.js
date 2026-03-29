1d5ee4bd515629d051505d30673e3a23a56e1283
Kritii <kritikumari53782@gmail.com>
2026-03-29 22:57:14 +0530

    added

---
+++ p5.js
@@ -0,0 +1,7 @@
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+exchangeIcon = document.querySelector(".exchange"),
+selectTag = document.querySelectorAll("select"),
+icons = document.querySelectorAll(".row i"),
+translateBtn = document.querySelector("button"),
+statusText = document.querySelector(".status");
